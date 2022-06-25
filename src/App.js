186b7d5c58f6523f2f2f11f49ac74fb0c7ea1616
ShanuDey <sd4shanudey@gmail.com>
2022-06-25 20:13:02 +0530

    install react bootstrap
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
